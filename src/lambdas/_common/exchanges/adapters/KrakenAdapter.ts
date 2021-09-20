638faff32bb0f5370a4f8ad1a4c217f2d33f02ea
Javier Marquez <javi@arqex.com>
2021-09-21 00:24:49 +0200

    Start working on kraken adapter

---
+++ src/lambdas/_common/exchanges/adapters/KrakenAdapter.ts
@@ -0,0 +1,11 @@
+import { ExchangeAdapter } from "../ExchangeAdapter";
+
+export default class KrakenAdapter implements ExchangeAdapter {
+	constructor( exchangeAccount: DbExchangeAccount ){
+
+	}
+	getPortfolio() {
+
+	}
+}
+
---
+++ src/lambdas/_common/exchanges/adapters/KrakenClient.ts
@@ -0,0 +1,54 @@
+interface KrakenClientOptions {
+	apiKey: string
+	privateKey: string
+}
+interface GetAssetInfoOptions {
+	assets: string[],
+	aclass: string
+}
+interface GetTradableAssetPairsOptions {
+	pairs: string[],
+	infoType?: 'all' | 'leverage' | 'fees' | 'margin'
+}
+interface OHLCOptions {
+	pair: string,
+	interval?: 1 | 5 | 15 | 30 | 60 | 240 | 1440 | 10080 | 21600
+	since?: number
+}
+interface OrderBookOptions {
+	pair: string,
+	count?: number,
+}
+interface RecentQueryOptions {
+	pair: string,
+	since?: number
+}
+export class KrakenClient {
+	static pub = {
+		getTime() {},
+		getSystemStatus(){},
+		getAssetInfo(options: GetAssetInfoOptions){},
+		getTradableAssetPairs(options: GetTradableAssetPairsOptions){},
+		getTickerInfo(pair: string){},
+		/** Return max 720 candles */
+		getOHLC(options: OHLCOptions){},
+		getOrderBook(options: OrderBookOptions){},
+		getRecentTrades(options: RecentQueryOptions){},
+		getRecentSpread(options: RecentQueryOptions){}
+	}
+
+	options: KrakenClientOptions
+	constructor(_options: KrakenClientOptions){
+		this.options = _options;
+	}
+
+	getAccountBalance() {
+
+	}
+
+	getTradeBalance() {
+
+	}
+
+	async get
+}
